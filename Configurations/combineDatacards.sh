786ce9837d38831a8da79e7180566ac9016af069
scodella <luca.scodellaro@cern.ch>
2018-08-20 10:21:20 +0200

    clean

--- Configurations/combineDatacards.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat MassPointList${1}.txt | while read line
-do
-   if [[ -n $line && "$line" != *"#"* ]]
-   then
-      ./combinePointDatacards.sh Datacards$2/MassPoint$line
-   fi
-done
--- Configurations/createDatacards.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat MassPointList${1}.txt | while read line
-do
-  if [[ -n $line && "$line" != *"#"* ]]
-  then
-    ./createPointDatacards.sh $line $2
-  fi
-done
