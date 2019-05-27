36173cf2da4af7322a17de5e70a7200277ab8191
scodella <luca.scodellaro@cern.ch>
2019-05-27 17:58:36 +0200

    clean

--- Configurations/SUS-17-010/MaxLikelihoodFit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-combine -M MaxLikelihoodFit --saveShapes Datacards/ValidationRegions/datacard$1.txt
-mv mlfit.root Datacards/ValidationRegions/mlfit$1.root
--- Configurations/SUS-17-010/purge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-echo "Purging " Datacards$1
-#
-rm -r Datacards$1/*/SR*
-rm -r Datacards$1/*/*.txt
-#
-
