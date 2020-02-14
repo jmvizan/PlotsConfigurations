94168fa5efee4cc82d3d30f980164291da2c4a0d
scodella <luca.scodellaro@cern.ch>
2020-02-14 10:57:08 +0100

    move tree to /eos/user

--- Configurations/SUS-19-XXX/samples.py
+++ Configurations/SUS-19-XXX/samples.py
@@ -16,12 +16,15 @@ print 'Value of lumi set to', opt.lumi
 SITE=os.uname()[1]
 
 if  'cern' in SITE :
-    treeBaseDirSig  = '/eos/cms/store/user/scodella/SUSY/Nano/'
-    treeBaseDirData = '/eos/cms/store/caf/user/scodella/BTV/Nano/'
-    if '2018' in opt.tag :
-        treeBaseDirMC   = '/eos/cms/store/caf/user/scodella/BTV/Nano/'
-    else :
-        treeBaseDirMC   = '/eos/cms/store/user/scodella/SUSY/Nano/'
+    treeBaseDirSig  = '/eos/user/s/scodella/SUSY/Nano/'
+    treeBaseDirData = '/eos/user/s/scodella/SUSY/Nano/'
+    treeBaseDirMC   = '/eos/user/s/scodella/SUSY/Nano/'
+    #treeBaseDirSig  = '/eos/cms/store/user/scodella/SUSY/Nano/'
+    #treeBaseDirData = '/eos/cms/store/caf/user/scodella/BTV/Nano/'
+    #if '2018' in opt.tag :
+    #    treeBaseDirMC   = '/eos/cms/store/caf/user/scodella/BTV/Nano/'
+    #else :
+    #    treeBaseDirMC   = '/eos/cms/store/user/scodella/SUSY/Nano/'
 elif 'ifca' in SITE:
     treeBaseDirSig  = '/gpfs/projects/tier3data/LatinosSkims/RunII/Nano/'
     treeBaseDirMC   = '/gpfs/projects/tier3data/LatinosSkims/RunII/Nano/'
