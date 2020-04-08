3d263f330f7bea192627d171b05141510d2ce884
scodella <luca.scodellaro@cern.ch>
2020-04-08 11:24:21 +0200

    switch to nAODv6

--- Configurations/SUS-19-XXX/configuration.py
+++ Configurations/SUS-19-XXX/configuration.py
@@ -12,8 +12,8 @@ variablesFile = 'variables.py'
 cutsFile = 'cuts.py' 
 
 # file with list of samples
-samplesFile = 'samples.py' 
-#samplesFile = 'samples_nanoAODv6.py' 
+#samplesFile = 'samples.py' 
+samplesFile = 'samples_nanoAODv6.py' 
 
 # file with the definition of the mass points for the signals
 #signalMassPointsFile = 'signalMassPoints.py'
