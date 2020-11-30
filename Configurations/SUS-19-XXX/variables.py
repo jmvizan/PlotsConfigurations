b2cc2cb3a3079f25ed8ec02a31193a6bd58b3d8a
Luca <luca.scodellaro@cern.ch>
2020-11-30 18:33:56 +0100

    more variable ttZ

--- Configurations/SUS-19-XXX/variables.py
+++ Configurations/SUS-19-XXX/variables.py
@@ -379,6 +379,25 @@ elif 'ttZNormalization' in opt.tag:
                              'fold'  : overflow                 #   fold overflow
                            }
  
+
+    variables['ptmissSR'] = {  'name'  : ptmissTTZ,             #   variable name
+                               'range' : ([0, 20, 40, 60, 80, 100, 120, 160, 220, 280, 380, 480],[1]), #   variable range
+                               'xaxis' : met + gv,                #   x axis name
+                               'fold'  : overflow                 #   fold overflow
+                             }
+
+    variables['njets']  = {  'name'  : 'nCleanJet',             #   variable name
+                             'range' : (  6,    0.,     6.),    #   variable range
+                             'xaxis' : 'number of jets',        #   x axis name
+                             'fold'  : overflow                 #   fold overflow
+                           }
+
+    variables['jets']   = {  'name'  : 'nCleanJet>0',           #   variable name
+                             'range' : (  2,    0.,     2.),    #   variable range 
+                             'xaxis' : 'number of jets',        #   x axis name
+                             'fold'  : overflow                 #   fold overflow
+                           }
+
 elif 'Validation' in opt.tag or 'Signal' in opt.tag:
 
     mt2ll = 'mt2ll' + ctrltag
