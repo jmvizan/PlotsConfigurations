382d16651c6cdf416c0920927e5643ffd81adabc
pmatorras <pablo.matorras@cern.ch>
2021-12-07 18:28:41 +0100

    add 2016 values

--- Configurations/SUS-19-XXX/samples_nanoAODv9.py
+++ Configurations/SUS-19-XXX/samples_nanoAODv9.py
@@ -437,9 +437,10 @@ for lep_i in ['Lep']:
 
 # nonprompt lepton rate TODO: update to UL values <- This is done, isnt it? I don't think so, not nAODv9 for sure
 
-if   '2016' in yeartag: nonpromptLep = { 'rate' : '1.23', 'rateUp' : '1.31', 'rateDown' : '1.15' } 
-elif '2017' in yeartag: nonpromptLep = { 'rate' : '1.38', 'rateUp' : '1.46', 'rateDown' : '1.29' } 
-elif '2018' in yeartag: nonpromptLep = { 'rate' : '1.28', 'rateUp' : '1.37', 'rateDown' : '1.21' } 
+if   '2016HIPM'   in yeartag: nonpromptLep = { 'rate' : '1.02', 'rateUp' : '1.11', 'rateDown' : '0.93' } 
+elif '2016noHIPM' in yeartag: nonpromptLep = { 'rate' : '1.00', 'rateUp' : '1.21', 'rateDown' : '0.81' } 
+elif '2017'       in yeartag: nonpromptLep = { 'rate' : '1.38', 'rateUp' : '1.46', 'rateDown' : '1.29' } 
+elif '2018'       in yeartag: nonpromptLep = { 'rate' : '1.28', 'rateUp' : '1.37', 'rateDown' : '1.21' } 
 if 'nonpromptSF' in opt.tag: # To check that mismodelling doesnt change much the limits
     if   '2016' in yeartag: nonpromptLep = { 'rate' : '1.00', 'rateUp' : '1.23', 'rateDown' : '0.77' } 
     elif '2017' in yeartag: nonpromptLep = { 'rate' : '1.00', 'rateUp' : '1.48', 'rateDown' : '0.52' } 
